31852a19a484317bf469e2c6c7cfedd55b1ea577
Teddy <dudetotheleft@gmail.com>
2023-07-17 17:17:42 -0700

    Add project files.

---
+++ Die.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DieRoller
+{
+    /// <summary>
+    /// Represents a single six-sided die (1 - 6)
+    /// </summary>
+    public class Die
+    {
+        /// <summary>
+        /// Represents the current face-up value
+        /// </summary>
+        public byte FaceValue { get; set; }
+
+        /// <summary>
+        /// Represents whether the die is held - True if the die is held
+        /// </summary>
+        public bool IsHeld { get; set; }
+
+        /// <summary>
+        /// Rolls the die and sets <see cref="FaceValue"/> to the new number.
+        /// Returns the new number.
+        /// </summary>
+        public void Roll()
+        {
+            // Generate random number
+            // Set FaceValue
+            // Return number
+
+            throw new NotImplementedException();
+        }
+    }
+}
